e7666bdc33bdfe477a2b70d941d927f6952c473b
fabge <geiger.fabian@outlook.com>
2021-05-11 16:57:15 +0200

    Update

--- boto3.sh
+++ boto3.sh
@@ -1,5 +1,4 @@
 cd boto3/docs
-make html
-doc2dash build/html --name boto3 --destination ./boto3-docs
-ls
-ls boto3-docs
+#make html
+#doc2dash build/html --name boto3 --destination ./boto3-docs
+doc2dash docs/_build/html/ --name boto3 --destination ./boto3-docs
