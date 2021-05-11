b4a41ceed04f905d3ce77b5e18845ebc05673509
fabge <geiger.fabian@outlook.com>
2021-05-11 19:55:56 +0200

    Update

--- requests/requests.sh
+++ requests/requests.sh
@@ -1,8 +1,8 @@
 mkdir tmp
 (cd tmp && git clone https://github.com/psf/requests.git)
 (cd docs && make html)
-doc2dash requests/docs/_build/html/ --name requests --destination .
+doc2dash requests/docs/_build/html/ --name requests --destination requests/
 git config --global user.email "docsets-bot@example.com"
 git config --global user.name "docsets-bot"
-git add requests.docset && git commit -m "Updated docsets"
+git add requests/requests.docset && git commit -m "Updated docsets"
 git push
