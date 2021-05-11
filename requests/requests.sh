df058d683a839efa53f1996039d2f5bf74c85437
fabge <geiger.fabian@outlook.com>
2021-05-11 19:55:05 +0200

    Update

--- requests/requests.sh
+++ requests/requests.sh
@@ -1,3 +1,6 @@
+mkdir tmp
+(cd tmp && git clone https://github.com/psf/requests.git)
+(cd docs && make html)
 doc2dash requests/docs/_build/html/ --name requests --destination .
 git config --global user.email "docsets-bot@example.com"
 git config --global user.name "docsets-bot"
