b5507117a8d45d1037ca58ab8d900f2f128ad8e4
fabge <geiger.fabian@outlook.com>
2021-05-11 16:25:58 +0200

    Update

--- boto3.sh
+++ boto3.sh
@@ -1,6 +1,7 @@
 git clone https://github.com/boto/boto3.git
 pip install -r requirements-docs.txt
 pip install --upgrade boto3
+pip install doc2dash
 cd docs
 make html
 doc2dash -A build/html --name boto3
